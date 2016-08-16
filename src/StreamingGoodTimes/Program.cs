7d61c9786269a34187976df38edb69d27c2327cf
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-08-17 07:40:00 +1000

    Initial commit

---
+++ src/StreamingGoodTimes/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Serialization;
+
+namespace StreamingGoodTimes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serverTask = Task.Run(() => RunServer());
+            var clientTask = Task.Run(() => RunClient());
+
+            Task.WhenAll(serverTask, clientTask).GetAwaiter().GetResult();
+            Console.WriteLine("All done");
+            Console.ReadKey();
+        }
+
+        const int NumWrites = 10;
+        const int MessageLength = 4096;
+
+        private static void RunServer()
+        {
+            var serializer = GetSerializer();
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 27111);
+            try
+            {
+                listener.Start(1);
+
+                using (var server = listener.AcceptTcpClientAsync().GetAwaiter().GetResult())
+                using (var stream = server.GetStream())
+                {
+                    for (var j = 0; j < NumWrites; j++)
+                        using (var zip = new DeflateStream(stream, CompressionMode.Compress, true))
+                        using (var buffer = new BufferedStream(zip))
+                        using(var bson = new BsonWriter(buffer) { CloseOutput = false})
+                        {
+                            Signal.WaitOne();
+                            Console.WriteLine($"writing message {j}");
+                            serializer.Serialize(bson, Envelope.Create(MessageLength));
+                            bson.Flush();
+                            Console.WriteLine($"message written {j}");
+                        }
+                }
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+
+        private static void RunClient()
+        {
+            var serializer = GetSerializer();
+            using (var client = new TcpClient())
+            {
+                client.ConnectAsync("localhost", 27111).GetAwaiter().GetResult();
+                client.ReceiveTimeout = 60000;
+                using (var stream = client.GetStream())
+                {
+                    for (var j = 0; j < NumWrites; j++)
+                    using (var zip = new DeflateStream(stream, CompressionMode.Decompress, true))
+                    using (var buffer = new BufferedStream(zip))
+                    using (var bson = new BsonReader(buffer) { CloseInput = false })
+                    {
+                        Console.WriteLine($"reading message {j}");
+                        var message = serializer.Deserialize<Envelope>(bson).Message;
+                        Console.WriteLine($"message read {j}");
+                        Signal.Set();
+                    }
+                }
+            }
+        }
+
+#region totes-region-bro
+        static readonly AutoResetEvent Signal = new AutoResetEvent(true);
+        
+        public class Envelope
+        {
+            public string Message { get; set; }
+
+            public static Envelope Create(int length)
+            {
+                return new Envelope
+                {
+                    Message = CreateString(length)
+                };
+            }
+        }
+
+        static JsonSerializer GetSerializer()
+        {
+            var serializer = JsonSerializer.Create();
+            serializer.Formatting = Formatting.None;
+            serializer.ContractResolver = new DefaultContractResolver();
+            serializer.TypeNameHandling = TypeNameHandling.Auto;
+            serializer.TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple;
+            serializer.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+            return serializer;
+        }
+
+        static Random rd = new Random();
+        internal static string CreateString(int stringLength)
+        {
+            const string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789!@$?_-";
+            char[] chars = new char[stringLength];
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+#endregion
+    }
+}
